9a98a6128a76068f8eac0dfdf77000df841934d2
Duy Nguyễn <nhatduy123n@gmail.com>
2025-06-25 18:23:30 +0700

    Thêm bai tap

---
+++ KTLT/Exercise/Linked list/Double linked list.cpp
@@ -0,0 +1,420 @@
+﻿#include <iostream>
+using namespace std;
+
+
+struct DNode {
+
+	int key;
+	DNode * pNext;
+	DNode * pPrev;
+};
+
+struct DList {
+
+	DNode * pHead;
+	DNode * pTail;
+};
+
+
+
+DNode* createNode(int data) {
+
+	DNode* newNode = new DNode;
+	newNode->key = data;
+	newNode->pNext = newNode->pPrev = NULL;
+
+	return newNode;
+}
+
+DList* createList(DNode* pNode) {
+
+	DList* newList = new DList;
+	newList->pHead = newList->pTail = pNode;
+
+	return newList;
+}
+
+
+bool addHead(DList*& L, int data) {
+
+	DNode* newNode = createNode(data);
+	if (!newNode) return false;
+	if (!L) {
+
+		L = createList(newNode);
+		return true;
+	}
+
+	newNode->pNext = L->pHead;
+	L->pHead->pPrev = newNode;
+	L->pHead = newNode;
+
+	return true;
+}
+
+
+
+bool addTail(DList*& L, int data) {
+
+	DNode* newNode = createNode(data);
+	if (!newNode) return false;
+
+	if (!L) {
+
+		L = createList(newNode);
+		return true;
+	}
+
+	L->pTail->pNext = newNode;
+	newNode->pPrev = L->pTail;
+	L->pTail = newNode;
+	return true;
+}
+
+bool addPos(DList*& L, int val, int pos) {
+
+	DNode* newNode = createNode(val);
+	if (!L || !newNode) return false;
+
+	DNode* prev = NULL;
+	DNode* temp = L->pHead;
+	int count = 0;
+
+	while (temp && (count < pos)) {
+
+		count++;
+		prev = temp;
+		temp = temp->pNext;
+	}
+
+	if (!temp && (count < pos)) return false;
+
+	if (prev) {
+
+		newNode->pNext = temp;
+		if (temp) temp->pPrev = newNode;
+		else L->pTail = newNode;
+		newNode->pPrev = prev;
+		prev->pNext = newNode;
+	}
+	else {
+
+		newNode->pNext = temp;
+		temp->pPrev = newNode;
+		L->pHead = newNode;
+	}
+
+	
+}
+
+
+
+bool addBefore(DList*& L, int data, int val) {
+
+
+	if (!L) return false;
+
+	DNode* prev = NULL;
+	DNode* temp = L->pHead;
+
+	while (temp) {
+
+		if (temp->key == val) {
+
+			DNode* newNode = createNode(data);
+			newNode->pNext = temp;
+			temp->pPrev = newNode;
+			newNode->pPrev = prev;
+
+			if (prev) prev->pNext = newNode;
+			else L->pHead = newNode;
+		}
+
+		prev = temp;
+		temp = temp->pNext;
+
+	}
+	return true;
+}
+
+
+bool addAfter(DList*& L, int data, int val) {
+
+	if (!L) return false;
+
+	DNode* temp = L->pHead;
+
+	while (temp) {
+
+		if (temp->key == val) {
+
+			DNode* newNode = createNode(data);
+			DNode* next = temp->pNext;
+
+			temp->pNext = newNode;
+			newNode->pPrev = temp;
+			newNode->pNext = next;
+
+			if (next) next->pPrev = newNode;
+			else L->pTail = newNode;
+
+			temp = next;
+		}
+
+		else {
+
+			temp = temp->pNext;
+		}
+	}
+	return true;
+}
+
+
+void removeHead(DList*& L) {
+
+	if (!L) return;
+
+	DNode* temp = L->pHead;
+	L->pHead = L->pHead->pNext;
+	L->pHead->pPrev = NULL;
+
+	temp->pNext = NULL;
+
+	delete temp;
+}
+
+
+void removeTail(DList*& L) {
+
+	if (!L) return;
+
+	DNode* temp = L->pTail;
+	L->pTail = L->pTail->pPrev;
+	L->pTail->pNext = NULL;
+
+	temp->pPrev = NULL;
+	delete temp;
+
+}
+
+
+void removePos(DList*& L, int pos) {
+
+	if (!L) return;
+
+	DNode* temp = L->pHead;
+	int count = 0;
+
+	while (temp && (count < pos )) {
+
+		count++;
+		temp = temp->pNext;
+	}
+
+	if (!temp || (count < pos)) return;
+
+	DNode* prev = temp->pPrev;
+	DNode* next = temp->pNext;
+
+	if (prev) prev->pNext = next;
+	else L->pHead = next;
+	if (next) next->pPrev = prev;
+	else L->pTail = prev;
+
+	delete temp;
+}
+
+
+void removeAll(DList*& L) {
+
+	if (!L) return;
+
+	while (L->pHead) {
+
+		DNode* temp = L->pHead;
+		L->pHead = L->pHead->pNext;
+		L->pHead->pPrev = NULL;
+
+		delete temp;
+	}
+
+	L->pHead = L->pTail = NULL;
+
+}
+
+
+void removeBefore(DList* L, int val) {
+
+	if (!L) return;
+
+	DNode* temp = L->pHead;
+	DNode* next = L->pHead->pNext;
+
+	while (temp && next) {
+
+		if (next->key == val) {
+
+			DNode* prev = temp->pPrev;
+			if (prev) prev->pNext = next;
+			else L->pHead = next;
+
+			next->pPrev = prev;
+			delete temp;
+
+		}
+
+		temp = next;
+		next = next->pNext;
+	}
+
+}
+
+void removeAfter(DList* L, int val) {
+
+	if (!L) return;
+	
+	DNode* temp = L->pHead->pNext;
+	DNode* prev = L->pHead;
+
+	while (temp) {
+
+		if (prev->key == val) {
+
+			DNode* next = temp->pNext;
+			prev->pNext = next;	
+			if (next) next->pPrev = prev;
+			else L->pTail = prev;
+
+			delete temp;
+
+			prev = next;
+			if (next) temp = next->pNext;
+			else temp = NULL;
+			
+		}
+		else {
+
+			prev = temp;
+			temp = temp->pNext;
+		}
+	}
+}
+
+int countElements(DList* L) {
+
+	if (!L) return 0;
+	int count = 0;
+
+	DNode* temp = L->pHead;
+	while (temp) {
+
+		count++;
+		temp = temp->pNext;
+	}
+
+	return count;
+}
+
+DList* reverseList(DList* L) {
+
+	if (!L) return NULL;
+
+	DList* newL = NULL;
+	DNode* temp = L->pHead;
+	while (temp) {
+
+		addHead(newL, temp->key);
+		temp = temp->pNext;
+	}
+
+	return newL;
+
+}
+
+
+void printList(DList* L) {
+
+	DNode* temp = L->pHead;
+	while (temp)
+	{
+		cout << temp->key << " ";
+		temp = temp->pNext;
+	}
+	cout << endl;
+	temp = L->pTail;
+	while (temp) {
+
+		cout << temp->key << " ";
+		temp = temp->pPrev;
+	}
+
+}
+
+void removeDuplicate(DList*& L) {
+
+	if (!L) return;
+
+	DNode* temp = L->pHead;
+
+	while (temp) {
+
+		DNode* temp2 = temp->pNext;
+		while (temp2) {
+
+			if (temp2->key == temp->key) {
+
+				DNode* prev = temp2->pPrev;
+				DNode* next = temp2->pNext;
+
+				prev->pNext = next;
+				if (next) next->pPrev = prev;
+				else L->pTail = prev;
+				
+				temp2 = next;
+			}
+			else {
+
+				temp2 = temp2->pNext;
+			}
+
+		}
+		temp = temp->pNext;
+
+	}
+
+}
+
+bool removeElement(DList*& L, int val) {
+
+	if (!L) return false;
+
+	DNode* temp = L->pHead;
+	while (temp) {
+
+		if (temp->key == val) {
+
+			DNode* prev = temp->pPrev;
+			DNode* next = temp->pNext;
+
+			if (prev) prev->pNext = next;
+			else L->pHead = next;
+			if (next) next->pPrev = prev;
+			else L->pTail = prev;
+
+			delete temp;
+
+			temp = next;
+		}
+		else {
+
+			temp = temp->pNext;
+		}
+	}
+}
+
+
+int main() {
+
+
+
+}
---
+++ KTLT/Exercise/Linked list/Single linked list.cpp
@@ -0,0 +1,424 @@
+﻿#include <iostream>
+using namespace std; 
+
+
+
+struct Node {
+
+	int key;
+	Node * pNext;
+};
+
+struct List {
+
+	Node * pHead;
+	Node * pTail;
+};
+
+Node* createNode(int data) {
+
+	Node* newNode = new Node;
+	newNode->key = data;
+	newNode->pNext = NULL;
+
+	return newNode;
+}
+
+
+List* createList(Node* pNode) {
+
+	List* newList = new List;
+	newList->pHead = pNode;
+	newList->pTail = pNode;
+
+	return newList;
+}
+
+
+bool addHead(List*& L, int data) {
+
+	Node* newNode = createNode(data);
+	if (!newNode) return false;
+	if (!L) {
+
+		L = createList(newNode);
+		return true;
+	}
+
+	newNode->pNext = L->pHead;
+	L->pHead = newNode;
+	return true;
+}
+
+
+bool addTail(List*& L, int data) {
+
+	Node* newNode = createNode(data);
+	if (!newNode) return false;
+	if (!L) {
+
+		L = createList(newNode);
+		return true;
+	}
+
+	L->pTail->pNext = newNode;
+	L->pTail = newNode;
+	return true;
+}
+
+bool addPos(List*& L, int val, int pos) {
+
+	Node* newNode = createNode(val);
+	if (!newNode || !L) return false;
+
+	Node* prev = NULL;
+	Node* temp = L->pHead;
+	int count = 0;
+
+	while (temp && (count < pos)) {
+
+		count++;
+		prev = temp;
+		temp = temp->pNext;
+	}
+
+	if (count < pos) return false;
+
+	if (prev) {
+
+		newNode->pNext = temp;
+		prev->pNext = newNode;
+	}
+	else {
+
+		newNode->pNext = L->pHead;
+		L->pHead = newNode;
+	}
+
+	if (prev == L->pTail) L->pTail = newNode;
+	return true;
+
+}
+
+
+bool addBefore(List*& L, int data, int val) {
+
+	if (!L) return false;
+
+	Node* prev = NULL;
+	Node* temp = L->pHead;
+
+	while (temp) {
+
+		if (temp->key == val) {
+
+			Node* newNode = createNode(data);
+			if (prev) {
+
+				newNode->pNext = temp;
+				prev->pNext = newNode;
+			}
+			else {
+
+				newNode->pNext = L->pHead;
+				L->pHead = newNode;
+			}
+
+		}
+
+		prev = temp;
+		temp = temp->pNext;
+	}
+	return true;
+}
+
+
+bool addAfter(List*& L, int data, int val) {
+
+	if (!L) return false;
+
+	Node* temp = L->pHead;
+
+	while (temp) {
+
+		if (temp->key == val) {
+
+			Node* newNode = createNode(data);
+			Node* next = temp->pNext;
+
+			if (temp->pNext) {
+
+				newNode->pNext = next;
+				temp->pNext = newNode;
+			}
+			else {
+
+				temp->pNext = newNode;
+				L->pTail = newNode;
+			}
+
+			temp = next;
+		}
+		else {
+
+			temp = temp->pNext;
+		}
+	}
+	return true;
+}
+
+
+
+void removeHead(List*& L) {
+
+	if (!L) return;
+
+	Node* temp = L->pHead;
+	L->pHead = L->pHead->pNext;
+	delete temp;
+
+	if (!L->pHead) L->pTail = NULL;
+}
+
+void removeTail(List*& L) {
+
+	if (!L) return;
+
+	Node* prev = NULL;
+	Node* temp = L->pHead;
+
+	while (temp != L->pTail) {
+
+		prev = temp;
+		temp = temp->pNext;
+	}
+
+	Node* deleteNode = temp;
+	temp = temp->pNext;
+	delete deleteNode;
+
+	if (!prev) L->pHead = NULL;
+
+}
+
+
+void removePos(List*& L, int pos) {
+
+	if (!L || pos < 0) return;
+
+	Node* prev = NULL;
+	Node* temp = L->pHead;
+	int count = 0;
+
+	while (temp && count < pos) {
+
+		count++;
+		prev = temp;
+		temp = temp->pNext;
+	}
+
+	if ((count < pos) || !temp) return;
+
+	if (prev) {
+
+		Node* next = temp->pNext;
+		prev->pNext = next;
+	}
+
+	else {
+
+		L->pHead = L->pHead->pNext;
+	}
+
+	if (temp == L->pTail) {
+
+		L->pTail = prev;
+	}
+	delete temp;
+
+}
+
+void removeAll(List*& L) {
+
+	if (!L) return;
+
+	while (L->pHead) {
+
+		Node* temp = L->pHead;
+		L->pHead = L->pHead->pNext;
+
+		delete temp;
+		
+	}
+	L->pHead = L->pTail = NULL;
+}
+
+
+void removeBefore(List* L, int val) {
+
+	if (!L) return;
+
+	Node* prev = NULL;
+	Node* temp = L->pHead;
+
+	while (temp && temp->pNext) {
+
+		if (temp->pNext->key == val) {
+
+			Node* next = temp->pNext;
+			if (prev) prev->pNext = next;
+			else L->pHead = next;
+
+			delete temp;
+			temp = next;
+		}
+		else {
+
+			prev = temp;
+			temp = temp->pNext;
+		}
+	}
+}
+
+
+void removeAfter(List* L, int val) {
+
+	if (!L) return;
+
+	Node* temp = L->pHead;
+
+	while (temp && temp->pNext) {
+
+		if (temp->key == val) {
+
+			Node* next = temp->pNext;
+			temp->pNext = next->pNext;
+
+			if (next == L->pTail) L->pTail = temp;
+
+			delete next;
+
+		}
+		temp = temp->pNext;
+	}
+}
+
+
+
+
+
+void printList(List *L) {
+
+	Node *temp = L->pHead;
+	while (temp) {
+
+		cout << temp->key << " ";
+		temp = temp->pNext;
+	}
+	cout << endl;
+}
+
+
+int countElements(List* L) {
+
+	int count = 0;
+
+	Node* temp = L->pHead;
+	while (temp) {
+
+		count++;
+		temp = temp->pNext;
+	}
+	return count;
+}
+
+List* reverseList(List* L) {
+
+	if (!L) return NULL;
+
+	List* newL = NULL;
+	Node* temp = L->pHead;
+
+	while (temp) {
+
+		addHead(newL, temp->key);
+		temp = temp->pNext;
+	}
+
+	return newL;
+
+}
+
+void removeDuplicate(List*& L) {
+
+	if (!L) return;
+	Node* temp = L->pHead;
+
+	while (temp) {
+
+		Node* prev = temp;
+		Node* temp2 = temp->pNext;
+
+		while (temp2) {
+
+			if (temp2->key == temp->key) {
+
+				Node* next = temp2->pNext;
+				prev->pNext = next;
+
+				if (!next) L->pTail = prev;
+				delete temp2;
+				temp2 = next;
+			}
+			else {
+
+				prev = temp2;
+				temp2 = temp2->pNext;
+			}
+
+		}
+		temp = temp->pNext;
+ 	}
+}
+
+
+
+bool removeElement(List*& L, int val) {
+
+	if (!L) return false;
+
+	Node* prev = NULL;
+	Node* temp = L->pHead;
+
+	while (temp) {
+
+		if (temp->key == val) {
+
+			Node* next = temp->pNext;
+			if (prev) prev->pNext = next;
+			else L->pHead = next;
+
+			if (!next) L->pTail = prev;
+			delete temp;
+			temp = next;
+		}
+		else {
+
+			prev = temp;
+			temp = temp->pNext;
+		}
+	}
+	return true;
+}
+
+
+int main() {
+
+	List* L = NULL;
+
+
+	
+
+
+	return 0;
+}
