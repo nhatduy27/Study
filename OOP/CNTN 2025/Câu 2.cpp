f1dd19581a3ce1dd01cdc11d74b3eb478d092d96
Duy Nguyễn <nhatduy123n@gmail.com>
2025-07-02 22:21:15 +0700

    them de thi 2025

---
+++ OOP/CNTN 2025/Câu 2.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+class Fraction {
+private:
+    int tu;
+    int mau;
+public:
+    Fraction() : tu(0), mau(1) {}
+    Fraction(int tu, int mau) : tu(tu), mau(mau){}
+    friend ostream& operator << (ostream& out, Fraction p) {
+        out << p.tu << "/" << p.mau;
+        return out;
+    }
+
+    int getTu() { return tu; }
+    int getMau() { return mau; }
+
+
+};
+
+class PrimeFracIterator {
+
+private:
+    Fraction* a;
+    int length;
+    int index;
+public:
+    PrimeFracIterator() : a(NULL), length(0), index(0) {}
+    PrimeFracIterator(Fraction a[], int length)
+        : a(a), length(length), index(0) {
+        
+        if ((index < length) &&
+            (!SoNguyenTo(a[index].getTu()) || !SoNguyenTo(a[index].getMau()))) {
+            index++;
+        }
+    
+    }
+
+    bool SoNguyenTo(int x) {
+
+        int count = 0;
+        for (int i = 1; i <= x; i++) {
+
+            if (x % i == 0) count++;
+        }
+        return count == 2;
+    }
+
+    operator bool() {
+        return index < length;
+    }
+
+    Fraction& operator*() {
+
+        return a[index];
+    }
+
+    void operator ++() {
+
+        index++;
+        if ((index < length) && 
+            (!SoNguyenTo(a[index].getTu()) || !SoNguyenTo(a[index].getMau()))) {
+            index++;
+        }
+    }
+
+};
+
+
+
+
+int main() {
+    Fraction a[] = { {1, 2}, {2, 3}, {4, 2}, {3, 5} };
+    int n = sizeof(a) / sizeof(a[0]);
+
+    PrimeFracIterator i(a, n);
+    while (i) {
+        std::cout << *i << " ";
+        ++i;
+    }
+
+    return 0;
+}
---
+++ OOP/CNTN 2025/Câu 3.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+
+
+//class Interface
+template <typename T>
+class Filter {
+public:
+    virtual bool execute(T element, int index, vector<T> array) = 0;
+};
+
+
+//Các class thực thi chức năng
+
+template <typename T>
+class DivisibleFilter : public Filter<T> {
+private:
+    int N;
+public:
+    DivisibleFilter(int N) : N(N){}
+    bool execute(T element, int index, vector<T> array);
+};
+
+
+template <typename T>
+class OddIndexFilter : public Filter<T> {
+
+public:
+    bool execute(T element, int index, vector<T> array);
+};
+
+
+template <typename T>
+class DiffAvgFilter : public Filter<T> {
+private:
+    double M;
+public:
+    DiffAvgFilter(double M) : M(M){}
+    bool execute(T element, int index, vector<T> array);
+};
+
+////class sử dụng filters
+
+template <typename T>
+class Extractor {
+
+    vector <Filter<T>*> filters;
+public:
+
+    void add(Filter<T>* p);
+    vector<T> extract(vector<T> &arr);
+};
+
+
+
+//Triển khai các hàm
+
+template <typename T>
+bool DivisibleFilter<T>::execute(T element, int index, vector<T> array) {
+
+    return element % N == 0;
+}
+
+template <typename T>
+bool OddIndexFilter<T>::execute(T element, int index, vector<T> array) {
+
+    return index % 2 != 0;
+}
+
+template <typename T>
+bool DiffAvgFilter<T>::execute(T element, int index, vector<T> array) {
+    if (array.empty()) return false;
+
+    double sum = 0;
+    for (int i = 0; i < array.size(); i++) {
+        sum += array[i];
+    }
+    double avg = sum / array.size();
+
+    double diff = abs(element - avg);
+    double threshold = (M) * avg;
+
+    return diff <= threshold;
+}
+
+
+
+template <typename T>
+void Extractor<T>::add(Filter<T>* p) {
+
+    filters.push_back(p);
+}
+
+
+template <typename T>
+vector<T> Extractor<T>::extract(vector<T>& arr) {
+    vector<T> result;
+
+    for (int i = 0; i < arr.size(); i++) {
+        bool check = true;
+
+        for (int j = 0; j < filters.size(); j++) {
+            if (!filters[j]->execute(arr[i], i, arr)) {
+                check = false;
+                break;
+            }
+        }
+
+        if (check) {
+            result.push_back(arr[i]);
+        }
+    }
+
+    return result;
+}
+
+
+int main() {
+
+    Extractor<int> ex;
+    ex.add(new DivisibleFilter<int>(3));
+    ex.add(new OddIndexFilter<int>());
+    ex.add(new DiffAvgFilter<int>(0.2));
+
+    std::vector<int> a = { 10, 3, 15, 6 , 2, 9, 4, 15, 5, 6 };
+    std::vector<int> b = ex.extract(a);
+
+    for (auto &e : b) {
+        std::cout << e << " ";
+    }
+    // Kết quả mong đợi: 9 6 9
+    return 0;
+}
+
+
+
