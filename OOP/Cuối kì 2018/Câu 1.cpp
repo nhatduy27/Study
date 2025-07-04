5b57249411882acdfa0f14a9d0f2446445bf97ad
Duy Nguyễn <nhatduy123n@gmail.com>
2025-07-04 09:05:01 +0700

    them giai de 2018

---
+++ OOP/Cuối kì 2018/Câu 1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+
+
+/*
+a)
+	Màn hình sẽ in ra :
+		B text
+		A default
+b)
+	=> Ban đầu chương trình sẽ khởi tạo đối tượng B mà gán m_s = "text" thông qua constructor của A, sau đó chương trình khai  báo con trỏ
+	A giữ địa chỉ của đối tượng con là B thông qua constructor sao chép của B
+	Gọi hàm foo các tham số A* obj1 đưa vào địa chỉ obj1, nhưng tham số A obj2 là đối tượng A do truyền bào kiểu tham trị
+	Khi thực thi thì obj1 in ra B text do prepare được override lại ở lớp B
+
+c)
+	Thêm hàm hủy virtual ở lớp A để 
+*/
+
+
+
+class A {
+private:
+	char* m_s;
+public:
+	A() { m_s = _strdup("default"); }
+	A(char* s) { m_s = s; }
+	virtual void prepare() { cout << "A "; }
+	void display() {
+		prepare();
+		cout << m_s << endl;
+	}
+
+	virtual ~A() {
+
+		if (!m_s) delete m_s;
+	}
+
+};
+class B : public A {
+public:
+	B(char* s) : A(s) { }
+	B(const B& b) {}
+	void prepare() { cout << "B "; }
+};
+void foo(A* obj1, A obj2) {
+	obj1->display();
+	obj2.display();
+}
+void main() {
+	char* s = _strdup("text");
+	B obj1(s);
+	A* obj2 = new B(obj1);
+	foo(&obj1, *obj2);
+
+
+	delete obj2;
+}
---
+++ OOP/Cuối kì 2018/Câu 2.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+//Dùng Strategy Design Pattern
+
+class Price {
+private:
+	float price;
+	float intelligence;
+	float strength;
+public:
+	Price(float price, float intelligence,float strength) 
+		:price(price), intelligence(intelligence), strength(strength) {}
+
+	float getPrice() { return price; }
+	float getIntelligence() { return intelligence; }
+	float getStrength() { return strength; }
+	void SubtractPrice(float x) { price -= x; }
+	void SubtractStrength(float x) { strength -= x; }
+
+
+};
+
+class Gate {
+
+public:
+	Gate(){}
+	virtual bool execute(Price &p) = 0;
+};
+
+
+
+class BusinessGate : public Gate {
+private:
+	float price;
+	int numbers;
+public:
+	BusinessGate(float price, int numbers) : price(price), numbers(numbers) {}
+
+	bool execute(Price& p) {
+
+		float totalPrice = price * numbers;
+		p.SubtractPrice(totalPrice);
+		if (p.getPrice() >= 0) {
+
+
+			return true;
+		}
+		return false;
+	}
+
+};
+
+
+class AcademicGate : public Gate {
+
+private:
+	float intelligence;
+public:
+	AcademicGate(int intelligence) : intelligence(intelligence) {}
+
+	bool execute(Price& p) {
+
+		return this->intelligence <= p.getIntelligence();
+	}
+
+};
+
+
+
+class PowerGate : public Gate {
+
+private:
+	float power;
+public:
+	PowerGate(int power) : power(power) {}
+	bool execute(Price& p) {
+
+		p.SubtractStrength(power);
+		if (p.getStrength() >= 0) return true;
+		return false;
+	}
+};
+
+
+
+class Castle {
+
+private:
+	vector<Gate*> gates;
+public:
+	Castle() { gates.clear(); }
+	void add(Gate* p) { gates.push_back(p); }
+
+	bool checking(Price &p) {
+
+		for (int i = 0; i < gates.size(); i++) {
+
+			if (!gates[i]->execute(p)) {
+
+				return false;
+			}
+		}
+
+
+		cout << "Hoang tu giai cuu thanh cong" << endl;
+		cout << "Money: " << p.getPrice() << endl;
+		cout << "Intelligence: " << p.getIntelligence() << endl;
+		cout << "Strength: " << p.getStrength() << endl;
+
+	}
+};
+
+
+int main() {
+
+	Castle p;
+	Gate* A = new BusinessGate(10.0, 10);
+	Gate *  B = new AcademicGate(100);
+	Gate* C = new PowerGate(150);
+
+	p.add(A);
+	p.add(B);
+	p.add(C);
+
+	Price D(101, 100, 160);
+	p.checking(D);
+
+
+	return 0;
+}
+
